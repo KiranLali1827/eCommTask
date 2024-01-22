0dd0e7eebcae8388cad284114e5d81fbeae5a593
Kiran <Kiranlali99@gmail.com>
2024-01-22 22:46:51 +0530

    UI Changed in Product Tab

--- src/pages/Products/Addproduct.js
+++ src/pages/Products/Addproduct.js
@@ -1,41 +1,54 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Imageupload from "./Imageupload";
-import { Box, Paper, TextField } from "@mui/material";
+import { Box, Grid, Paper, TextField } from "@mui/material";
 import "./Addproduct.scss";
 import { Button } from "evergreen-ui";
 import Createproductsection from "./Createproductsection";
+import SegmentedControl from "../users/SegmentedControl";
 
 function Addproduct() {
+  const [selectedValue1, setSelectedValue1] = useState("Create Employee");
+
   return (
     <div>
       <div className="home">
         <Sidebar />
         <div className="homeContainer">
           <Navbar />
-
-          <Paper
-            style={{ backgroundColor: "gray", marginTop: "15px" }}
-            className="Add_Product_Top"
-          >
-            <br></br>
-            <p
-              style={{
-                fontSize: "25px",
-                textAlign: "center",
-                color: "white",
-                fontFamily: "sans-serif",
-                marginTop: "-6px",
-              }}
-            >
-              Add Products and Details
+          
+          <div>
+            <SegmentedControl
+              name="group-1"
+              callback={(val) => setSelectedValue1(val)}
+              controlRef={useRef()}
+              segments={[
+                {
+                  label: "Add Product",
+                  value: "Add Product",
+                  ref: useRef(),
+                },
+                {
+                  label: "View Product",
+                  value: "View Product",
+                  ref: useRef(),
+                },
+              ]}
+            />
+            <p className="selected-item">
+              {
+                <div>
+                  {selectedValue1 == "Add Product" ? "Add Product" : ""}
+                  {selectedValue1 == "View Product" ? "View Product" : ""}
+                </div>
+              }
             </p>
-          </Paper>
+          </div>
 
           <div className="Image_Textfields_Parameter_Section">
             <Paper className="ImageSection">
-             
+              <Imageupload />
             </Paper>
             <Paper className="Products_Paramter_Section">
               <div>
@@ -48,39 +61,22 @@ function Addproduct() {
             <Paper className="Products_Description_Section">
               <h2>Product Description</h2>
               <label>
-                <textarea className="Border_Color"
+                <textarea
+                  className="Border_Color"
                   placeholder="Description......."
                   style={{ marginTop: "15px" }}
                   name="postContent"
-                  rows={7}
+                  rows={5}
                   cols={159}
-                
                 />
               </label>
             </Paper>
           </div>
-
-          <Paper
-            style={{ backgroundColor: "gray", marginTop: "8px" }}
-            className="Add_Product_Top"
-          >
-            <br></br>
-            <p
-              style={{
-                fontSize: "25px",
-                textAlign: "center",
-                color: "white",
-                fontFamily: "sans-serif",
-                marginTop: "-6px",
-              }}
-            >
-              Preview Details
-            </p>
-          </Paper>
-
-          <div></div>
-
-          {/* <Imageupload /> */}
+          <div>
+            <Button>
+              Preview and Save
+            </Button>
+          </div>
         </div>
       </div>
     </div>
